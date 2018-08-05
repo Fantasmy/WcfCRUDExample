51a5ab5ef8f01ff7f8c21336c7cebf1147b71951
Jenny <fantazmit@gmail.com>
2018-08-05 15:15:21 +0200

    Created DTO class. Close #5.

---
+++ WcfServiceApp/WcfDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfServiceApp
+{
+    [DataContract]
+    public class WcfDto
+    {
+        [DataMember]
+        public System.Guid id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string surname { get; set; }
+        [DataMember]
+        public string email { get; set; }
+    }
+}
