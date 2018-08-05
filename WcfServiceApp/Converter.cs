b916cec205ad0bfc699110c774c0ff9e5744746b
Jenny <fantazmit@gmail.com>
2018-08-05 15:36:30 +0200

    Created Converter class. Close #6.

---
+++ WcfServiceApp/Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfServiceApp
+{
+    public static class Converter
+    {
+        public static WcfDto ConvertWCFtoWCFDTO(Students obj)
+        {
+            WcfDto objdto = new WcfDto();
+            objdto.id = obj.id;
+            objdto.name = obj.name;
+            objdto.surname = obj.surname;
+            objdto.email = obj.email;
+            return objdto;
+        }
+
+        public static Students ConvertWCFDTOtoWCF(WcfDto obj)
+        {
+            Students objdto = new Students();
+            objdto.id = obj.id;
+            objdto.name = obj.name;
+            objdto.surname = obj.surname;
+            objdto.email = obj.email;
+            return objdto;
+        }
+    }
+}
