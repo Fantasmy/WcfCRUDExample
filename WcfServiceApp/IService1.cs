6db593f41a1f2d2721d93c1535331ef437383cf2
Jenny <fantazmit@gmail.com>
2018-08-05 15:44:31 +0200

    Created contract CRUD. Close #7.

--- WcfServiceApp/IService1.cs
+++ WcfServiceApp/IService1.cs
@@ -8,40 +8,19 @@ using System.Text;
 
 namespace WcfServiceApp
 {
-    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
     public interface IService1
     {
 
         [OperationContract]
-        string GetData(int value);
-
+        WcfDto Find(string empid);
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: agregue aquí sus operaciones de servicio
-    }
-
-
-    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
+        List<WcfDto> All();
+        [OperationContract]
+        void InsertData(WcfDto obj);
+        [OperationContract]
+        void Updatedata(WcfDto obj);
+        [OperationContract]
+        void DeleteData(WcfDto obj);
     }
 }
